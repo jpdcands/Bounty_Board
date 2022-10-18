e2ce90ca726bbb4b4376d04ac88955afb28fbef5
Paul <jpdcands211@gmail.com>
2022-10-18 18:40:29 -0400

    Second Commit pg. 125

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,64 @@
+const val HERO_NAME = "Madrigal"
+var playerLevel = 8
+
+fun main() {
+    println("$HERO_NAME announces her presence to the world.")
+    println("What level is $HERO_NAME?")
+    val playerLevelInput = readLine()!!
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
+        playerLevelInput.toInt()
+    } else {
+        1
+    }
+    println("$HERO_NAME's level is $playerLevel.")
+
+    readBountyBoard()
+
+    println("Time passes...")
+    println("$HERO_NAME returns from her quest.")
+
+    playerLevel += 1
+    println(playerLevel)
+    readBountyBoard()
+}
+
+private fun readBountyBoard() {
+    val quest: String? = obtainQuest(playerLevel)
+    val censoredQuest: String? = quest?.replace("Nogartse", "xxxxxxxx")
+
+    if (censoredQuest != null) {
+        println(
+            """
+            |$HERO_NAME approaches the bounty board.  It reads:
+            |    "$censoredQuest"
+            |""".trimMargin()
+        )
+    }
+}
+
+    private fun obtainQuest(
+        playerLevel: Int,
+        playerClass: String = "paladin",
+        hasBefriendedBarbarians: Boolean = true,
+        hasAngeredBarbarians: Boolean = false
+    ): String? = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            // Check whether diplomacy is an option
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else -> null
+    }
+
+
